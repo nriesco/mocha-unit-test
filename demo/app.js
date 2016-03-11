155b1c6abe377301d5b6c331ca288f5842b5bb9e
nriesco <riesco@gmail.com>
2016-03-10 21:24:03 -0300

    se agrega ejemplo de aplicación que usa la librería

---
+++ demo/app.js
@@ -0,0 +1,7 @@
+var ManejoString = require('mocha-unit-test').ManejoString;
+
+var texto = 'Hola';
+console.log('Bajando el case de:\n', texto, '>>', ManejoString.minusculaPrimeraLetra(texto));
+
+// texto = null;
+// console.log('Bajando el case de:\n', texto, '>>', ManejoString.minusculaPrimeraLetra(texto));
